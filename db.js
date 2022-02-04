6f244801d75b9437448f1903eb2825011bbb5740
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-02-04 15:01:53 +0600

    testing purpose

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://afride:afride123@cluster0.qzuor.mongodb.net/anotebook?retryWrites=true&w=majority";
+// const mongoURI = "mongodb+srv://afride:afride123@cluster0.qzuor.mongodb.net/anotebook?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://afride:afride123@cluster0.qzuor.mongodb.net/anotebook?retryWrites=true&w=majority"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
